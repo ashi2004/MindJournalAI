c26b1f15ee986eeacc8cf54d41a696bfdd0198bd
ashi2004 <135041623+ashi2004@users.noreply.github.com>
2025-09-29 09:48:54 +0530

    Time: 2 ms (99.07%) | Memory: 41.7 MB (17.96%) - LeetSync

--- 1111-minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.java
+++ 1111-minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.java
@@ -1,33 +1,23 @@
 class Solution {
+    public int minScore(int[] v, int i, int j, int[][] dp) {
+        if (j - i <= 1) return 0;
+        if (j - i == 2) return v[i] * v[i + 1] * v[j];
+        if (dp[i][j] != -1) return dp[i][j];
 
-    int n;
-    int[] values;
-    Map<Integer, Integer> memo = new HashMap<Integer, Integer>();
-
-    public int minScoreTriangulation(int[] values) {
-        this.n = values.length;
-        this.values = values;
-        return dp(0, n - 1);
+        int score = Integer.MAX_VALUE;
+        for (int k = i + 1; k < j; k++) {
+            score = Math.min(score , minScore(v , i , k , dp) + v[i] * v[k] * v[j] + minScore(v , k , j , dp));
+        }
+        return dp[i][j] = score;
     }
 
-    public int dp(int i, int j) {
-        if (i + 2 > j) {
-            return 0;
+    public int minScoreTriangulation(int[] values) {
+        int n = values.length;
+        int[][] dp = new int[n][n];
+
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
         }
-        if (i + 2 == j) {
-            return values[i] * values[i + 1] * values[j];
-        }
-        int key = i * n + j;
-        if (!memo.containsKey(key)) {
-            int minScore = Integer.MAX_VALUE;
-            for (int k = i + 1; k < j; k++) {
-                minScore = Math.min(
-                    minScore,
-                    values[i] * values[k] * values[j] + dp(i, k) + dp(k, j)
-                );
-            }
-            memo.put(key, minScore);
-        }
-        return memo.get(key);
+        return minScore(values , 0 , n - 1 , dp);
     }
 }
